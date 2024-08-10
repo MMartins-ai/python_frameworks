dcb55cb74b84ad879ff4a266aa9049e2ef0e7541
MMartins-ai <mateusorigin2268@gmail.com>
2024-08-09 21:14:26 -0300

    Criando vectorDB

---
+++ langchain/langchain_intro/create_retriever.py
@@ -0,0 +1,18 @@
+import dotenv
+#from langchain.document_loaders.csv_loader import CSVLoader #Depreciada
+from langchain_community.document_loaders import CSVLoader #Nova versão
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+
+REVIEWS_CSV_PATH = "../data/reviews.csv"
+REVIEWS_CHROMA_PATH = "chroma_data"
+
+dotenv.load_dotenv('../.env')
+
+loader = CSVLoader(file_path=REVIEWS_CSV_PATH, source_column="review")
+
+reviews = loader.load()
+
+reviews_vector_db = Chroma.from_documents(
+    reviews, OpenAIEmbeddings(), persist_directory=REVIEWS_CHROMA_PATH
+)
